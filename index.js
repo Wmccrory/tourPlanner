b6534df316f78ee7472543e2ea43d7ffac0397b1
Will <wmccrory@gmail.com>
2018-04-11 13:07:27 -0500

    Forgot to assign variable DOH

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ app.get('/', (req, res) => {
 	res.send({Hello: 'there'});
 })
 const PORT = process.env.PORT || 5000;
-app.listen(5000, () => {
+app.listen(PORT, () => {
 	console.log(`Now listening on port ${PORT}`);
 });
